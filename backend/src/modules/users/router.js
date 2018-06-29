73a2c6eb42e64cf4f270cbea71106df2fcd42c66
Yuri <snicksnk@MacBook-Pro-Yuri.local>
2018-06-30 00:33:46 +0400

    Removed unneded user methods

--- backend/src/modules/users/router.js
+++ backend/src/modules/users/router.js
@@ -14,38 +14,4 @@ export default [
       user.createUser
     ]
   },
-  {
-    method: 'GET',
-    route: '/',
-    handlers: [
-      ensureUser,
-      user.getUsers
-    ]
-  },
-  {
-    method: 'GET',
-    route: '/:id',
-    handlers: [
-      ensureUser,
-      user.getUser
-    ]
-  },
-  {
-    method: 'PUT',
-    route: '/:id',
-    handlers: [
-      ensureUser,
-      user.getUser,
-      user.updateUser
-    ]
-  },
-  {
-    method: 'DELETE',
-    route: '/:id',
-    handlers: [
-      ensureUser,
-      user.getUser,
-      user.deleteUser
-    ]
-  }
 ];
